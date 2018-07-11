33db76d98a703f92a6b4749646d56576f350111d
H Sharath Achar <sharathachar110@gmail.com>
2018-07-11 12:32:39 +0530

    chained all functions and used es6 arrow function without using return keyword

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,14 +1,6 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = (arr) => {
-  // splitting the string
-  const spl = arr.split('');
-  // reversing the string
-  const rvr = spl.reverse('');
-  // joining the string
-  const str = rvr.join('');
-  // returns the reversed string
-  return str;
-};
+const reverse = arr => arr.split('').reverse('').join('');
+
 
 module.exports = reverse;
